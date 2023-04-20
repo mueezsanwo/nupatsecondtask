0871685e1e008e396f6fc9b9c6510e97fa676bac
mueez sanwo <mueezsanwo@gmail.com>
2023-04-20 16:11:36 +0100

    new commit

--- src/components/BodyCards.jsx
+++ src/components/BodyCards.jsx
@@ -5,7 +5,7 @@ import Card3 from './Card3'
 
 const BodyCards = () => {
   return (
-    <div className=' flex justify-evenly absolute  l:bg-green-400 2xl:bg-red-500 l:absolute 2xl:absolute bg-[#FCFBFC] w-[fill] left-[272px] top-[261px] h-[stretch]'>
+    <div className=' flex justify-evenly absolute  l:absolute 2xl:absolute bg-[#FCFBFC] w-[fill] left-[272px] top-[261px] h-[stretch]'>
         <div className=' mt-[64px] h-[661px] flex gap-[40px] 2xl:gap-16 '>
            <Card1  />
            <Card2 />
