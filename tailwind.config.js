e8697ebbe6b3d5b82cbfd499b0dae43841b3b450
mueez sanwo <mueezsanwo@gmail.com>
2023-04-24 10:35:06 +0100

    added comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,10 @@ export default {
       screens: {  
           'sm': '750px',
          ...defaultTheme.screens, 
-         'l': {'min': '1280px', 'max': '1439px'},
-        '2xl': {'min': '1440px', 'max': '1700px'},
+         'l': {'min': '360px', 'max': '1439px'},
+        //  from a screen width of 3600px(because there is no mobile screen design) through 1280px to 1439px
+        '2xl': {'min': '1440px', 'max': '1919px'},
+        // from a screen width of 1439px to 1919px (from 1919px and above would take the default screen)
       },
       dropShadow: {
         '3xl': '0px 0px 100px rgba(0, 0, 0, 0.02)',
