a775e4af60f5b0f6aa63a74c0a1f61dc6cef8329
mueez sanwo <mueezsanwo@gmail.com>
2023-04-27 12:33:11 +0100

    comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ export default {
          ...defaultTheme.screens, 
          'l': {'min': '360px', 'max': '1439px'},
         //  from a screen width of 3600px(because there is no mobile screen design) through 1280px to 1439px
-        '2xl': {'min': '1440px', 'max': '1919px'},
-        // from a screen width of 1439px to 1919px (from 1919px and above would take the default screen)
+        '2xl': {'min': '1440px', 'max': '1700px'},
+        // from a screen width of 1439px to 1700px (from 1700px and above would take the default screen)
       },
       dropShadow: {
         '3xl': '0px 0px 100px rgba(0, 0, 0, 0.02)',
